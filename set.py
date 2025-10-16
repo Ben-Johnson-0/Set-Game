9130b31472681cc494e3cda57f19b4a698943be0
Ben Johnson <143573847+Ben-Johnson-0@users.noreply.github.com>
2025-10-15 20:49:54 -0400

    Added player interaction functions
    Added functions for a player to be able to play the game.
    Renamed check_set to is_set and added select_card and submit_selection functions.

--- set.py
+++ set.py
@@ -37,22 +37,6 @@ def generate_deck() -> np.array:
     deck = np.array(list(product(range(N_VARIANTS), repeat=N_ATTRIBUTES)), dtype=np.int8)
     return deck
 
-def check_set(potential_set: np.array) -> bool:
-    """
-    Checks if an np.array of shape (N_VARIANTS, N_ATTRIBUTES) is a set.
-    A set is defined as N_VARIANTS cards where each attribute is either all the same or all different.
-    """
-    if potential_set.shape != (N_VARIANTS, N_ATTRIBUTES):
-        print(f"{__name__} defaulting to return False. Expected potential_set to be of shape {(N_VARIANTS, N_ATTRIBUTES)}, but received shape {potential_set.shape}.")
-        return False
-
-    # The sum of each attribute (column) must be evenly divisible by the number of variants attributes
-    sums = np.sum(potential_set, axis=0)
-    mods = np.mod(sums, N_VARIANTS)
-
-    # If all of the mod operations return 0 then it's a set
-    return (np.sum(mods) == 0)
-
 
 class SetGame:
     def __init__(self, board_size:int = 12):
@@ -61,8 +45,26 @@ class SetGame:
 
         self.cards_drawn = board_size
         self.board = self.deck[:self.cards_drawn]
+        self.selected = []  # Card indices selected by a player
+        self.matches = 0
 
     
+    def is_set(self, potential_set: np.array) -> bool:
+        """
+        Checks if an np.array of shape (N_VARIANTS, N_ATTRIBUTES) is a set.
+        A set is defined as N_VARIANTS cards where each attribute is either all the same or all different.
+        """
+        if potential_set.shape != (N_VARIANTS, N_ATTRIBUTES):
+            print(f"{__name__} defaulting to return False. Expected potential_set to be of shape {(N_VARIANTS, N_ATTRIBUTES)}, but received shape {potential_set.shape}.")
+            return False
+
+        # The sum of each attribute (column) must be evenly divisible by the number of variants attributes
+        sums = np.sum(potential_set, axis=0)
+        mods = np.mod(sums, N_VARIANTS)
+
+        # If all of the mod operations return 0 then it's a set
+        return (np.sum(mods) == 0)
+
     def find_set(self, new_indices: list | None = None) -> tuple:
         """
         Searches the board array for a set.
@@ -108,12 +110,14 @@ class SetGame:
                     for x in group[j+1:]:
                         cards_checked += 1
                         potential_set = np.array([initial_card, self.board[k], self.board[x]])
-                        if check_set(potential_set):
+                        if self.is_set(potential_set):
                             return True, np.array([i, k, x]), cards_checked
 
         return False, None, cards_checked
 
     def replace_cards(self, indices: list):
+        """Replace given card indices on the board with new cards"""
+
         # If there are no cards left in the deck, delete the indices instead of drawing
         cards_left :int = len(self.deck) - self.cards_drawn
         if len(indices) > cards_left - len(indices):
@@ -126,7 +130,7 @@ class SetGame:
         self.cards_drawn += len(indices)
 
     def play_self(self):
-        matches =  0
+        """Automatically plays through an entire game of Set and prints statistics"""
         checks = 0
         print("Starting board\n", self.board)
 
@@ -134,16 +138,37 @@ class SetGame:
             isFound, indices, n_checked = self.find_set()
             checks += n_checked
             if not isFound:
-                print(f"Failed to find set in {checks} tries. Found {matches} matches total.")
+                print(f"Failed to find set in {checks} tries. Found {self.matches} matches total.")
                 return
-            matches += 1
+            self.matches += 1
             self.replace_cards(indices)
             print("Match:", indices)
         
         print("Final board\n", self.board)
-        print(f"Found {matches} through {checks} card checks.")
+        print(f"Found {self.matches} through {checks} card checks.")
+
+    def select_card(self, idx):
+        """Toggles selection of a card on the board"""
+        if idx in self.selected:
+            self.selected.remove(idx)
+        
+        elif len(self.selected) < N_VARIANTS:
+            self.selected.append(idx)
+    
+    def submit_selection(self):
+        """Checks whether a selection of cards is valid, returns that value and updates the game state"""
+        if len(self.selected) != N_VARIANTS:
+            return False
+
+        cards = np.array(self.board[self.selected])
+        if self.is_set(cards):
+            self.matches += 1
+            self.replace_cards(self.selected)   # Replace indices rather than values
+            self.selected.clear()
+            return True
         
 
 if __name__ == "__main__":
+    print("Run gui.py for a user-friendly interface.")
     game = SetGame()
     game.play_self()
